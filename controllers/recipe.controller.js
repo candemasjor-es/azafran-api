fcc4fff2efc7f2afe112eb1dee37b72c35606e0b
Camilo Mascarell Jorques <camilomascarelljorques@gmail.com>
2025-07-04 16:40:57 +0200

    new

--- controllers/recipe.controller.js
+++ controllers/recipe.controller.js
@@ -1,9 +1,60 @@
+const { Recipe } = require("../models/recipe.model");
+const { Ingredient } = require("../models/ingredient.model");
+
+const { sendMessage } = require("../services/openai");
+
 const getRecipes = async (req, res) => {
-  // #swagger.tags = ['Recipes']
-  // #swagger.summary = 'Para guardar Recipes'
-  res.send("Recipes by saved ingredients");
+    // #swagger.tags = ['Recipes']
+    // #swagger.summary = 'To get AI generated recipes'
+    try {
+        let { ingredients } = req.query;
+        if (!ingredients) {
+            res.status(404).send({ msg: "No ingredients passed" });
+        }
+        if (!Array.isArray(ingredients)) {
+            ingredients = [ingredients];
+        }
+        // ingredients is an array of ids
+        const ingredientDocs = await Ingredient.find({
+            _id: { $in: ingredients },
+        });
+        const ingredientNames = ingredientDocs.map((ing) => ing.name);
+        const generatedRecipes = await sendMessage(ingredientNames);
+        res.send({ recipes: generatedRecipes });
+    } catch (error) {
+        // TODO: Improve error handling
+        console.log(error);
+        res.status(404).send({ error: error.name, message: error._message });
+    }
 };
 
-module.exports = {
-  getRecipes,
+const getUserRecipes = async (req, res) => {
+    // #swagger.tags = ['Recipes']
+    // #swagger.summary = 'To get user stored recipes'
+    try {
+        const userRecipes = await Recipe.find({ userId: req.user.id });
+        res.status(202).send(userRecipes);
+    } catch (error) {
+        console.log(error);
+        res.status(404).send({ error: error.name, message: error._message });
+    }
 };
+
+const createRecipe = async (req, res) => {
+    // #swagger.tags = ['Recipes']
+    // #swagger.summary = 'To store a recipe into the database'
+    try {
+        const createdRecipe = new Recipe({
+            ...req.body,
+            userId: req.user._id,
+        });
+        await createdRecipe.save();
+        res.status(201).send({ msg: "Recipe created" });
+    } catch (error) {
+        // TODO: Improve error handling
+        console.log(error);
+        res.status(404).send({ msg: error.name, message: error._message });
+    }
+};
+
+module.exports = { getRecipes, getUserRecipes, createRecipe };
--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const {
+    getRecipes,
+    getUserRecipes,
+    createRecipe,
+} = require("../controllers/recipe.controller");
 const router = express.Router();
-const { getRecipes } = require("../controllers/recipe.controller");
 
+router.get("/user", getUserRecipes);
 router.get("/", getRecipes);
+router.post("/", createRecipe);
 
 module.exports = router;
